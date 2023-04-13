74d4c6e747b813b7f410de36f809d29002f905dc
Willian Moreira Finamore <wmfinamore@hotmail.com>
2023-04-13 09:53:05 -0300

    Alterando atributos de elementos de input de dados

--- hello/input.py
+++ hello/input.py
@@ -8,6 +8,15 @@ def main(page: ft.Page):
     
     page.add(txt1, nome1)
     
+    txt2 = ft.Text('Valor final: ', size = 16)
+    val2 = ft.TextField(label = 'Digite o valor inteiro',
+                        prefix_text = 'R$ ')
+    
+    txt3 = ft.Text('Desconto aplicado: ', size = 16)
+    val3 = ft.TextField(label = 'Digite o percentual',
+                        suffix_text = '%')
+    page.add(txt2, val2, txt3, val3)
+    
     page.update()
     
 ft.app(target = main)
