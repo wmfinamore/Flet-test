4d4b367c9cbcbf3551b4e81bf5cc38f3ea4352bb
Willian Moreira Finamore <wmfinamore@hotmail.com>
2023-04-14 16:41:41 -0300

    Exemplo usando o elemento ResponsiveRow do Flet

--- hello/column.py
+++ hello/column.py
@@ -13,10 +13,11 @@ def main(page: ft.Page):
     
     bloco1 = ft.Row(controls = [titulo1])
     bloco2 = ft.Row(controls = [nome1, sobrenome1])
-    bloco3 = ft.Row(controls = [
-        ft.Column(controls = [data_nasc1]),
-        ft.Column(controls = [ft.Row(controls = [rg1, cpf1])])])
-    
+    bloco3 = ft.ResponsiveRow(controls = [
+        ft.Column(controls = [data_nasc1],
+                  col = {'sm': 6, 'md': 4, 'xl':2}),
+        ft.Column(controls = [ft.Row(controls = [rg1, cpf1])],
+                  col = {'sm': 6, 'md': 4, 'xl': 2})])
     
     page.add(bloco1, bloco2, bloco3)
     
